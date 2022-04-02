fd3847ea24666ff03447acd3b7f8dde0427ce787
Mark Dabler <mark@tackleai.com>
2022-04-02 10:45:57 -0500

    add outputs, change config, and add requirements

--- code/reddit/config.py
+++ code/reddit/config.py
@@ -4,6 +4,6 @@ HF_MODEL_NEWS_CLASSIFIER = "mrm8488/bert-mini-finetuned-age_news-classification"
 SUBREDDITS = "wallstreetbets+investing+options+pennystocks+options+stocks"
 NEWS_SUBREDDITS = "news"
 
-NUM_SUBMISSION_TO_GET = 1000
+NUM_SUBMISSION_TO_GET = 10
 
 NEWS_CLASSES = ["world", "sports", "business", "sci/tech"]
